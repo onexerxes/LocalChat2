1e56cf62772c9286d56d14e84be3d5a41dbb30d1
Xerxes Dotiwalla <onexerxes@hotmail.com>
2015-07-18 22:07:54 -0400

    Initial load

---
+++ LocalChat/LocalChat.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using LocalChat.WinPhone.Resources;
+
+namespace LocalChat.WinPhone {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
